8c4e19d125ad4b216f8c62b3b2b1667aa06afec8
Jonathan Russ <jonathan.russ@bluewin.ch>
2023-05-01 18:16:56 +0200

    chore(component): update AutoCompleteTokenInput

--- src/components/AutoCompleteTokenInput.tsx
+++ src/components/AutoCompleteTokenInput.tsx
@@ -27,18 +27,16 @@ function AutoCompleteTokenInput({ notes }: any) {
   function getColorCircle(color: any) {
     return function () {
       return (
-        <>
-          <Box
-            bg={color}
-            borderColor={color}
-            width={14}
-            height={14}
-            borderRadius={10}
-            margin="auto"
-            borderWidth="1px"
-            borderStyle="solid"
-          />
-        </>
+        <Box
+          bg={color}
+          borderColor={color}
+          width={14}
+          height={14}
+          borderRadius={10}
+          margin="auto"
+          borderWidth="1px"
+          borderStyle="solid"
+        />
       );
     };
   }
@@ -85,161 +83,72 @@ function AutoCompleteTokenInput({ notes }: any) {
   };
 
   return (
-    <>
-      <FormControl>
-        <FormControl.Label id="autocompleteLabel-customRenderedItem">
-          Pick labels
-        </FormControl.Label>
-        <Autocomplete>
-          <Autocomplete.Input
-            as={TextInputWithTokens}
-            tokens={tokens}
-            tokenComponent={IssueLabelToken}
-            onTokenRemove={onTokenRemove}
+    <FormControl>
+      <FormControl.Label id="autocompleteLabel-customRenderedItem">
+        Pick labels
+      </FormControl.Label>
+      <Autocomplete>
+        <Autocomplete.Input
+          as={TextInputWithTokens}
+          tokens={tokens}
+          tokenComponent={IssueLabelToken}
+          onTokenRemove={onTokenRemove}
+        />
+        <Autocomplete.Overlay>
+          <Autocomplete.Menu
+            items={[
+              {
+                leadingVisual: getColorCircle('#a2eeef'),
+                text: 'enhancement',
+                id: 1,
+                metadata: { fillColor: '#a2eeef' },
+              },
+              {
+                leadingVisual: getColorCircle('#d73a4a'),
+                text: 'bug',
+                id: 2,
+                metadata: { fillColor: '#d73a4a' },
+              },
+              {
+                leadingVisual: getColorCircle('#0cf478'),
+                text: 'good first issue',
+                id: 3,
+                metadata: { fillColor: '#0cf478' },
+              },
+              {
+                leadingVisual: getColorCircle('#ffd78e'),
+                text: 'design',
+                id: 4,
+                metadata: { fillColor: '#ffd78e' },
+              },
+              {
+                leadingVisual: getColorCircle('#ff0000'),
+                text: 'blocker',
+                id: 5,
+                metadata: { fillColor: '#ff0000' },
+              },
+              {
+                leadingVisual: getColorCircle('#a4f287'),
+                text: 'backend',
+                id: 6,
+                metadata: { fillColor: '#a4f287' },
+              },
+              {
+                leadingVisual: getColorCircle('#8dc6fc'),
+                text: 'frontend',
+                id: 7,
+                metadata: { fillColor: '#8dc6fc' },
+              },
+            ]}
+            selectedItemIds={selectedItemIds}
+            onSelectedChange={onSelectedChange}
+            selectionVariant="multiple"
+            aria-labelledby="autocompleteLabel-customRenderedItem"
           />
-          <Autocomplete.Overlay>
-            <Autocomplete.Menu
-              items={[
-                {
-                  leadingVisual: getColorCircle('#a2eeef'),
-                  text: 'enhancement',
-                  id: 1,
-                  metadata: { fillColor: '#a2eeef' },
-                },
-                {
-                  leadingVisual: getColorCircle('#d73a4a'),
-                  text: 'bug',
-                  id: 2,
-                  metadata: { fillColor: '#d73a4a' },
-                },
-                {
-                  leadingVisual: getColorCircle('#0cf478'),
-                  text: 'good first issue',
-                  id: 3,
-                  metadata: { fillColor: '#0cf478' },
-                },
-                {
-                  leadingVisual: getColorCircle('#ffd78e'),
-                  text: 'design',
-                  id: 4,
-                  metadata: { fillColor: '#ffd78e' },
-                },
-                {
-                  leadingVisual: getColorCircle('#ff0000'),
-                  text: 'blocker',
-                  id: 5,
-                  metadata: { fillColor: '#ff0000' },
-                },
-                {
-                  leadingVisual: getColorCircle('#a4f287'),
-                  text: 'backend',
-                  id: 6,
-                  metadata: { fillColor: '#a4f287' },
-                },
-                {
-                  leadingVisual: getColorCircle('#8dc6fc'),
-                  text: 'frontend',
-                  id: 7,
-                  metadata: { fillColor: '#8dc6fc' },
-                },
-              ]}
-              selectedItemIds={selectedItemIds}
-              onSelectedChange={onSelectedChange}
-              selectionVariant="multiple"
-              aria-labelledby="autocompleteLabel-customRenderedItem"
-            />
-          </Autocomplete.Overlay>
-        </Autocomplete>
-      </FormControl>
-    </>
+        </Autocomplete.Overlay>
+      </Autocomplete>
+    </FormControl>
   );
-
-  // const selectedTokenIds = tokens.map((token) => token.id);
-  // const [selectedItemIds, setSelectedItemIds] =
-  //   React.useState(selectedTokenIds);
-  // const onTokenRemove = (tokenId: string) => {
-  //   setTokens(tokens.filter((token) => token.id !== tokenId));
-  //   setSelectedItemIds(selectedItemIds.filter((id) => id !== tokenId));
-  // };
-  // const onSelectedChange = (newlySelectedItems) => {
-  //   if (!Array.isArray(newlySelectedItems)) {
-  //     return;
-  //   }
-
-  //   setSelectedItemIds(newlySelectedItems.map((item) => item.id));
-
-  //   if (newlySelectedItems.length < selectedItemIds.length) {
-  //     const newlySelectedItemIds = newlySelectedItems.map(({ id }) => id);
-  //     const removedItemIds = selectedTokenIds.filter(
-  //       (id) => !newlySelectedItemIds.includes(id)
-  //     );
-
-  //     for (const removedItemId of removedItemIds) {
-  //       onTokenRemove(removedItemId);
-  //     }
-
-  //     return;
-  //   }
-
-  //   setTokens(newlySelectedItems.map(({ id, text }) => ({ id, text })));
-  // };
-
-  // return (
-  // <Box as="form" sx={{ p: 3 }}>
-  //   <AnchoredOverlay
-  //     open={isOpen}
-  //     onOpen={handleOpen}
-  //     onClose={() => setIsOpen(false)}
-  //     width="large"
-  //     height="xsmall"
-  //     focusTrapSettings={{ initialFocusRef: inputRef }}
-  //     side="inside-top"
-  //     renderAnchor={(props) => <Button {...props}>open overlay</Button>}
-  //   >
-  //     <Autocomplete>
-  //       <Box display="flex" flexDirection="column" height="100%">
-  //         <Box
-  //           borderWidth={0}
-  //           borderBottomWidth={1}
-  //           borderColor="border.default"
-  //           borderStyle="solid"
-  //         >
-  //           <Autocomplete.Input
-  //             ref={inputRef}
-  //             sx={{
-  //               display: 'flex',
-  //               border: '0',
-  //               paddingX: 3,
-  //               paddingY: 1,
-  //               boxShadow: 'none',
-  //               ':focus-within': {
-  //                 border: '0',
-  //                 boxShadow: 'none',
-  //               },
-  //             }}
-  //             {...textInputArgs}
-  //             size={textInputArgs.inputSize}
-  //             block
-  //           />
-  //         </Box>
-  //         <Box
-  //           overflow="auto"
-  //           flexGrow={1}
-  //           ref={scrollContainerRef as RefObject<HTMLDivElement>}
-  //         >
-  //           <Autocomplete.Menu
-  //             items={items}
-  //             selectedItemIds={[]}
-  //             customScrollContainerRef={scrollContainerRef}
-  //             aria-labelledby="autocompleteLabel"
-  //             {...menuArgs}
-  //           />
-  //         </Box>
-  //       </Box>
-  //     </Autocomplete>
-  //   </AnchoredOverlay>
-  // </Box>
-  // );
 }
 
 export default AutoCompleteTokenInput;
